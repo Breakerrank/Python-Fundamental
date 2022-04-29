ad77580938c7599dd05a7e79ce3d953c60c62d7f
Administrator <fachrudin.ds1@gmail.com>
2022-04-29 15:41:18 +0700

    looping update

---
+++ basic_syntax_looping_while_undefined_count.py
@@ -0,0 +1,25 @@
+"""
+Program perulangan dengan while sampai faham
+"""
+buku = 10
+print('Ibu berkata: "Baca semua bukumu"')
+jumlah_baca = 0
+
+jumlah_faham = 0
+print(f'Jumlah buku yang sudah dibaca {jumlah_faham}')
+
+while jumlah_baca < buku * 2:
+    jumlah_baca = jumlah_baca + 1
+    if jumlah_faham == 9:
+        print(f'Buku ke {jumlah_faham + 1} belum faham')
+    else:
+        jumlah_faham = jumlah_faham + 1
+        print(f'Buku ke {jumlah_faham} sudah dibaca dan difahami')
+
+print(f'Jumlah buku yang sudah dibaca dan difahami {jumlah_faham}')
+if jumlah_faham == buku:
+    print(f'"Bu, semua buku sudah dibaca dan difahami"')
+else:
+    print(f'"Bu, tidak semua buku bisa difahami. '
+          f'Budi hanya bisa memahami {jumlah_faham} buku"')
+
