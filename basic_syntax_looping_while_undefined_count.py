94d3a22415b5215db28f5d2635276082a25deed6
Fachru <fachrudin.ds1@gmail.com>
2022-12-12 15:34:36 +0700

    new update

--- basic_syntax_looping_while_undefined_count.py
+++ basic_syntax_looping_while_undefined_count.py
@@ -5,21 +5,21 @@ buku = 10
 print('Ibu berkata: "Baca semua bukumu"')
 jumlah_baca = 0
 
-jumlah_faham = 0
-print(f'Jumlah buku yang sudah dibaca {jumlah_faham}')
+jumlah_baca_dan_faham = 0
+print(f'Jumlah buku yang sudah dibaca {jumlah_baca_dan_faham}')
 
 while jumlah_baca < buku * 2:
     jumlah_baca = jumlah_baca + 1
-    if jumlah_faham == 9:
-        print(f'Buku ke {jumlah_faham + 1} belum faham')
+    if jumlah_baca_dan_faham == 7:
+        print(f'Buku ke {jumlah_baca_dan_faham + 1} belum faham')
     else:
-        jumlah_faham = jumlah_faham + 1
-        print(f'Buku ke {jumlah_faham} sudah dibaca dan difahami')
+        jumlah_baca_dan_faham = jumlah_baca_dan_faham + 1
+        print(f'Buku ke {jumlah_baca_dan_faham} sudah dibaca dan difahami')
 
-print(f'Jumlah buku yang sudah dibaca dan difahami {jumlah_faham}')
-if jumlah_faham == buku:
+print(f'Jumlah buku yang sudah dibaca dan difahami {jumlah_baca_dan_faham}')
+if jumlah_baca_dan_faham == buku:
     print(f'"Bu, semua buku sudah dibaca dan difahami"')
 else:
     print(f'"Bu, tidak semua buku bisa difahami. '
-          f'Budi hanya bisa memahami {jumlah_faham} buku"')
+          f'Budi hanya bisa memahami {jumlah_baca_dan_faham} buku"')
 
